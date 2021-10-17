6dece22604cbc4ca4aaeb3c05d44b98c12c44eb5
Aidansieg <aidansieg@gmail.com>
2021-10-17 16:02:00 -0400

    I think I finished Player class

--- player.py
+++ player.py
@@ -8,7 +8,11 @@ class Player:
         self.price = Player.price
         self.position = Player.get_position
         self.nationality = Player.get_nationality
-        self.team = set()
+        self.team = Player.get_team
+
+    def to_string(self):
+        s = self.name + ' age =' + str(self.age) + '; team =' + self.team + '; position =' + self.position + '; nationality =' + self.nationality + '; price =' + str(self.price)
+        return s
 
     def get_name(self):
         return self.name
@@ -18,3 +22,12 @@ class Player:
 
     def get_nationality(self):
         return self.nationality
+
+    def get_team(self):
+        return self.team
+
+    def get_age(self):
+        return self.age
+
+    def get_price(self):
+        return self.price
