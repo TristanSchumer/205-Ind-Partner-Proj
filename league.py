323ac6fd5b2410074239a7b8b8e2cb1818a6673f
Aidansieg <aidansieg@gmail.com>
2021-10-22 14:17:46 -0400

    Created do_transfer. I think this will work but its giving me an indentation error on prev_team even though it looks correct?

--- league.py
+++ league.py
@@ -4,8 +4,7 @@ import player
 
 class League:
     league = None
-    obj = Player()
-    te
+    obj = Player("")
 
     @classmethod
     def get(self):
@@ -55,10 +54,26 @@ class League:
 		    s = t.get_player() + ' => ' + t.get_team() + 'for $'
 		    print(s)
 
-    # can we pass in a Player object instead of just the name of a player?
-    def do_transfer(self, obj, new_team):
-        prev_team = obj.get_team()
-        print(prev_team)
+    # to do a transfer, we need two different teams, a player, the players price, and both of teams budgets to be affected by transfer
+	# if a team doesnt have enough money for player, the budget cannot go through
+    def do_transfer(self, obj, team):
+		prev_team = obj.get_team()
+		new_team = team
+		player_price = obj.get_price()
+		prev_team_budget = prev_team.get_transfer_budget()
+		new_team_budget = new_team.get_transfer_budget()
+
+		if new_team_budget <= player_price:
+			print("%s does not have enough a large enough transfer budget to afford %s", new_team, obj.get_name())
+
+		else:
+			new_team_budget = new_team_budget - player_price
+			prev_team_budget = prev_team_budget + player_price
+			obj.team = new_team.get_name()
+			# add player to teams set of players
+			print("%s has transfered to %s from %s for %i", obj.get_name(), new_team.get_name(), prev_team.get_name(), obj.get_price())
+    
+		return None
 
 
 
